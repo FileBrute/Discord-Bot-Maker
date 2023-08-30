acecde5a9a2720f541c351e9df6f7ea0cf1ccc11
FileBrute <88153990+FileBrute@users.noreply.github.com>
2023-08-30 23:09:02 +0100

    added files

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Discord Bot Maker
- Python/Requests
+# Discord Bot Creator
+
+Create a Discord bot application with ease using this simple Python script.
+
+## Features
+
+- Interactive console interface.
+- Rainbow-colored and centered banner for style.
+- Easily input your bot's name and token.
+- Sends a POST request to create the Discord bot application.
+
+## Getting Started
+
+### Prerequisites
+
+- Python 3.x
+- Requests library (you can install it via `pip install requests`)
+
+### Usage
+
+1. Clone the repository or download the script.
+2. Open a terminal or command prompt.
+3. Navigate to the script's directory.
+
+```bash
+cd path/to/discord-bot-creator
+```
+
+```bash
+python discord_bot_creator.py
+```
---
+++ main.py
@@ -0,0 +1,53 @@
+import requests
+
+
+def rainbow_text(text):
+    colors = [31, 33, 32, 36, 35, 34]
+    rainbow = ""
+    for i, char in enumerate(text):
+        color = colors[i % len(colors)]
+        rainbow += f"\x1b[{color}m{char}\x1b[0m"
+    return rainbow
+
+
+def center_text(text, width=80):
+    return text.center(width)
+
+
+banner_text = "Discord Bot Creator"
+
+
+rainbow_banner = rainbow_text(banner_text)
+
+
+terminal_width = 80
+
+
+centered_banner = center_text(rainbow_banner, terminal_width)
+
+
+print(centered_banner)
+
+
+name = input("Enter bot name: ")
+token = input("Enter token: ")
+
+s
+url = "https://discord.com/api/v9/applications"
+
+
+payload = {
+    "name": name
+}
+
+
+headers = {
+    "Authorization": token
+}
+
+r = requests.post(url, json=payload, headers=headers)
+
+if r.status_code == 201:
+    print("Bot application created successfully!")
+else:
+    print(f"Failed to create bot application. Status code: {r.status_code}")
